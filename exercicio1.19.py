cd50f39d868711f89b7044f0a0f636a26ad1f4ae
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-08-30 14:24:42 -0300

    Feat: exercicio1.19 :rocket:

---
+++ exercicio1.19.py
@@ -0,0 +1,6 @@
+#Conversao de litros para metros cubicos
+#Formula m = l/100
+
+l = float(input("Digite em valor em litros: "))
+m3 = l/1000
+print("O valor convertido em metros cubicos: ", '%.f' % m3,"m³")

af00d6584f804b313e5e715e27f6e60d42578741
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-08-30 14:25:43 -0300

    Feat: exercicio1.29 :rocket:

---
+++ exercicio1.29.py
@@ -0,0 +1,7 @@
+#Media aritmética das notas
+n1 = float(input("informe a nota 1: "))
+n2 = float(input("informe a nota 2: "))
+n3 = float(input("informe a nota 3: "))
+n4 = float(input("informe a nota 4: ")) 
+media = (n1+n2+n3+n4) / 4
+print( "A média aritmética é", media)

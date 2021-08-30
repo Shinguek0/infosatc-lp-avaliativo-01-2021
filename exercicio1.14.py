f3c650559ce025fa32fd6523373c975b4c67fb17
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-08-30 14:24:30 -0300

    Feat: exercicio1.14 :rocket:

---
+++ exercicio1.14.py
@@ -0,0 +1,6 @@
+#Ler um ângulo em graus e converte em radianos
+#Formula R = G*pi/180    (sendo pi = 3.14)
+
+g = float(input("Digite um ângulo em graus: "))
+r = g*3.14/180
+print("O ângulo convertido para radianos é: %.2f"%r)

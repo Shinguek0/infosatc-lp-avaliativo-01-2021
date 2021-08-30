af4657cc9cd955456f6b35d8f0c534bdc914853a
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-08-30 14:25:33 -0300

    Feat: exercicio1.28 :rocket:

---
+++ exercicio1.28.py
@@ -0,0 +1,9 @@
+#leia 3 valores e some o quadrado deles
+v1 = int(input("Digite um valor: ")) 
+v2 = int(input("Digite outro valor: "))
+v3 = int(input("Digite mais um valor: "))
+
+v1 = v1**2
+v2 = v2**2
+v3 = v3**2
+print("A soma dos quadrados dos numeros digitados é:", v1+v2+v3)

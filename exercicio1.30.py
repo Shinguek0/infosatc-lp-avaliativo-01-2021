81c0059f192c54764b3ca2f0206886869543298a
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-08-30 14:25:53 -0300

    Feat: exercicio1.30 :rocket:

---
+++ exercicio1.30.py
@@ -0,0 +1,4 @@
+#O programa le um valor em reias, e a cotaçao do dólar, e imprime o valor em dólares
+reais = float(input("Digite o valor em reais: "))
+cotacaoDolar = float(input("Digite a Cotação do Dólar: "))
+print("O valor em dólares é: ", reais / cotacaoDolar)

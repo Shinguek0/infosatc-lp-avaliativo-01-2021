53b4c24dd119cef191e10c127e5c18cd045d36e7
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-08-30 14:42:09 -0300

    Feat: exercicio2 :rocket:

---
+++ exercicio2.py
@@ -0,0 +1,22 @@
+"""Faça um programa para calcular a quantidade de latas de tintas para pintar uma
+parede. O programa deverá solicitar ao usuário, a altura e o comprimento da
+parede. Considere que a cobertura da tinta é de 1 litro para cada 3 metros
+quadrados e que a tinta é vendida em latas de 3,6 litros, que custam R$ 107,00.
+Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço
+total."""
+#3,6 litros, que custam R$ 107,00  (tinta)
+
+#importando math para arrendondar a quantdade de latas
+import math
+
+largura = int(input("Digite a largura da parede(em metros quadrados): "))
+altura = int(input("Digite a altura da parede(em metros quadrados): "))
+
+areaParede = largura*altura
+tinta = areaParede/3
+
+qndLatas = tinta/3.6
+
+
+print("Altura {}m², Largura {}m², Area da parede {}m²".format(altura,largura,areaParede))
+print("A quantidade de lata tinta necessária para pintar a parede é: " + str(math.ceil(qndLatas)) +", e o preço total é R$",math.ceil(qndLatas)*107.00)

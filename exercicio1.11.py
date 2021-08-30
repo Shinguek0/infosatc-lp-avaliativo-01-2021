ed38ba3b512068072563da829e3c35faf454a7e3
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-08-30 14:24:18 -0300

    Feat: exercicio1.11 :rocket:

---
+++ exercicio1.11.py
@@ -0,0 +1,6 @@
+#Conversao de m/s em Km/h
+#Formula K = m*3.6
+
+m = float(input("Digite a velocidade em metros por segundo: "))
+k = m*3.6
+print("A velocidade em quilômetros por hora é: ", k, "Km/h")

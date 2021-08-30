ed1e77d62ae2e23cded8af44481934f8f726fd22
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-08-30 14:24:50 -0300

    Feat: exercicio1.21 :rocket:

---
+++ exercicio1.21.py
@@ -0,0 +1,6 @@
+  
+#Atividade de conversao de libras para quilogramas
+#Formula k = l*0.45
+l = float(input("Digite o valor da massa que libras: "))
+k = l*0.45
+print("O valor da massa em quilogramas é: ", '%.2f' % k)
